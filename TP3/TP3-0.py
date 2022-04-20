4e3f0d1cd8ff14cd9520fc62d1edba5ea6c6ba99
Tokaa7 <ksoares@outlook.fr>
2022-04-21 01:01:55 +0200

    Update TP3-0.py

--- TP3/TP3-0.py
+++ TP3/TP3-0.py
@@ -20,27 +20,34 @@ def termesu(q,alpha,epsilon):
         cpt+=1
     return liste
 
+def termesu2(q,alpha,epsilon):
+    u=q**(alpha**0)
+    uu=q**(alpha**1)
+    liste=[u,uu]
+    n=2;cpt=0
+    while(abs(uu-u)>epsilon):
+        if(cpt>100):return liste
+        u=uu
+        uu=q**(alpha**n)
+        n+=1
+        liste.append(uu)
+        cpt+=1
+    return liste
+
 eps=0.01
 
-print("\n",termesu(0.9,1.1,eps))
-y1=termesu(0.9,1.1,eps)
+print("\nTermesu 1 :",termesu(0.9,3,eps))
+y1=termesu(0.9,3,eps)
 x1=range(len(y1))
-
+"""
 print("\n",termesu(0.9,3,eps))
-y2=termesu(0.9,3,eps)
+print("\n",termesu(0.3,3,eps))
+print("\n",termesu(0.3,1.1,eps))
+"""
+
+print("\nTermesu 2 :",termesu2(0.9,3,eps))
+y2=termesu2(0.9,3,eps)
 x2=range(len(y2))
 
-print("\n",termesu(0.3,3,eps))
-y3=termesu(0.3,3,eps)
-x3=range(len(y3))
-
-print("\n",termesu(0.3,1.1,eps))
-y4=termesu(0.3,1.1,eps)
-x4=range(len(y4))
-
-plt.plot(x1,y1,'r')
-plt.plot(x2,y2,'b')
-plt.plot(x3,y3,'g')
-plt.plot(x4,y4,'y')
-
-#q5
+plt.plot(x1,y1,'b')
+plt.plot(x2,y2,'r')
